0b49812bd304592abaf9bcd56cc433fb6b09f475
trushev <trushev.alex@gmail.com>
2021-02-27 17:43:25 +0700

    task2-1 - sieve of eratosthenes

--- src/clojure_course/task2_1/sieve.clj
+++ src/clojure_course/task2_1/sieve.clj
@@ -1,19 +1,24 @@
 (ns clojure-course.task2-1.sieve)
 
-(defn prime? [n primes]
-  (let [[prime? new-primes] (reduce
-                             (fn [acc val]
-                               (let [[prime? new-primes] acc
-                                     [prime comb]        val]
-                                 (if (= n comb)
-                                   (list false (conj new-primes (list prime (+ prime comb))))
-                                   (list prime? (conj new-primes val)))))
-                             (list true [])
-                             primes)]
-    (list prime?
-          (if prime?
-            (conj new-primes (list n (* n n)))
-            new-primes))))
+(defn test-prime [n primes]
+  (let [[prime?
+         new-primes] (reduce
+                      (fn [acc val]
+                        (let [[prime? new-primes] acc
+                              [prime comb]        val]
+                          (if (= n comb)
+                            [false (conj new-primes [prime (+ prime comb)])]
+                            [prime? (conj new-primes val)])))
+                      [true []]
+                      primes)]
+    (if prime?
+      (conj new-primes [n (* n n)])
+      new-primes)))
 
 (defn prime [nth]
-  (loop [i 0]))
+  (loop [cnt       2
+         primes    []]
+    (let [new-primes (test-prime cnt primes)]
+      (if (= (count new-primes) nth)
+        (first (peek new-primes))
+        (recur (inc cnt) new-primes)))))
--- test/clojure_course/task2_1/sieve_test.clj
+++ test/clojure_course/task2_1/sieve_test.clj
@@ -2,7 +2,10 @@
   (:require [clojure.test :refer :all]
             [clojure-course.task2-1.sieve :refer :all]))
 
-(deftest test-prime
-  (is (= 2 (prime 2)))
+(deftest test-nth-prime
+  (is (= 2 (prime 1)))
+  (is (= 3 (prime 2)))
   (is (= 7 (prime 4)))
-  (is (= 29 (prime 10))))
+  (is (= 29 (prime 10)))
+  (is (= 127 (prime 31)))
+  (is (= 263 (prime 56))))
