43f475bf3267614f596242dd14ca2a750feb1701
trushev <trushev.alex@gmail.com>
2021-04-14 17:17:11 +0700

    task4-1 - cnf

--- src/clojure_course/task4_1/cnf.clj
+++ src/clojure_course/task4_1/cnf.clj
@@ -1,7 +1,7 @@
 (ns clojure-course.task4-1.cnf)
 
 (defn expr?
-  "Tests whether expr is a expression"
+  "Tests whether expr is an expression"
   [expr]
   {:post [(boolean? %)]}
   (and
@@ -54,13 +54,6 @@
   {:pre  [(variable? var)]
    :post [(keyword? %)]}
   (second var))
-
-(defn same-variable?
-  "Tests whether var1 and var2 are the same"
-  [var1 var2]
-  {:pre  [(expr? var1) (expr? var2)]
-   :post [(boolean? %)]}
-  (= (variable-name var1) (variable-name var2)))
 
 (defn And
   "Creates a conjunction"
@@ -130,16 +123,45 @@
       (expr? (args expr))
       (variable? (args expr)))))
 
-(defn elem-disj?
-  "Tests whether expr is a disjunction of literals"
+(defn atom?
+  "Tests whether expr is a constant or a literal"
   [expr]
   {:pre  [(expr? expr)]
    :post [(boolean? %)]}
   (or
-    (literal? expr)
+    (const? expr)
+    (literal? expr)))
+
+(defn not-atom?
+  "Tests whether expr is not a atom"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (not (atom? expr)))
+
+(defn elementary?
+  [expr pred]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (or
+    (atom? expr)
     (and
-      (Or? expr)
-      (every? literal? (args expr)))))
+      (pred expr)
+      (every? atom? (args expr)))))
+
+(defn elem-disj?
+  "Tests whether expr is a disjunction of atoms"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (elementary? expr Or?))
+
+(defn elem-conj?
+  "Tests whether expr is a conjunction of atoms"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (elementary? expr And?))
 
 (defn cnf?
   "Tests whether expr is in conjunctive normal form"
@@ -151,6 +173,74 @@
     (and
       (And? expr)
       (every? elem-disj? (args expr)))))
+
+(defn not-cnf?
+  "Tests whether expr is in conjunctive normal form"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (not (cnf? expr)))
+
+(defn impless?
+  "Tests whether expr does not contain an implication"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (cond
+    (atom? expr) true
+    (impl? expr) false
+    (Not? expr) (impless? (args expr))
+    :default (every? impless? (args expr))))
+
+(defn not-impless?
+  "Tests whether expr does not contain an implication"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (not (impless? expr)))
+
+(defn nnf?
+  "Tests whether expr is in negation normal form"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (cond
+    (and (Not? expr) (not-atom? expr)) false
+    (atom? expr) true
+    :default (every? nnf? (args expr))))
+
+(defn not-nnf?
+  "Tests whether expr is not in negation normal form"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (not (nnf? expr)))
+
+(defn type-and-inner-type?
+  [expr outer-type inner-type]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (and
+    (outer-type expr)
+    (some? (some inner-type (args expr)))))
+
+(defn assoc-disj?
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (type-and-inner-type? expr Or? Or?))
+
+(defn assoc-conj?
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (type-and-inner-type? expr And? And?))
+
+(defn distributive?
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (type-and-inner-type? expr Or? And?))
 
 (defn expr-str
   "Represents expr as a string"
@@ -165,28 +255,6 @@
     (And? expr) (str "(" (clojure.string/join " & " (map #(expr-str %) (args expr))) ")")
     (impl? expr) (str "(" (expr-str (first (args expr))) " -> " (expr-str (second (args expr))) ")")))
 
-(defn impl-as-disj
-  "Represents implication as disjunction: (x -> y) = (!x | y)"
-  [expr]
-  {:pre  [(impl? expr)]
-   :post [(Or? %)]}
-  (let [[x y] (args expr)]
-    (Or (Not x) y)))
-
-(defn de-morgan-disj
-  "Represents negation of disjunction as conjunction of negations: !(x | y) = (!x & !y)"
-  [expr]
-  {:pre  [(Not? expr) (Or? (args expr))]
-   :post [(And? %)]}
-  (apply And (map #(Not %) (args (args expr)))))
-
-(defn de-morgan-conj
-  "Represents negation of conjunction as disjunction of negations: !(x & y) = (!x | !y)"
-  [expr]
-  {:pre  [(Not? expr) (And? (args expr))]
-   :post [(Or? %)]}
-  (apply Or (map #(Not %) (args (args expr)))))
-
 (defn flatten-by-pred
   [pred expr]
   {:pre [(or
@@ -197,21 +265,319 @@
     (mapcat #(flatten-by-pred pred %) (args expr))
     (list expr)))
 
-(defn associative-disj-law
+(defn assoc-disj-law
   "Allows removal of brackets from disjunction: (x | (y | z)) = (x | y | z)"
   [expr]
   {:pre  [(Or? expr)]
    :post [(Or? %)]}
   (apply Or (flatten-by-pred Or? expr)))
 
-(defn associative-conj-law
+(defn assoc-conj-law
   "Allows removal of brackets from conjunction: (x & (y & z)) = (x & y & z)"
   [expr]
   {:pre  [(And? expr)]
    :post [(And? %)]}
   (apply And (flatten-by-pred And? expr)))
 
+(defn not-and?
+  "Tests whether expr os not and operation"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (not (And? expr)))
 
-(defn -main []
-  (let [expr (Or (Or (variable :x) (variable :y) (And (variable :z) (variable :w))) (variable :v) (impl (variable :m) (variable :n)))]
-    (println (expr-str (associative-disj-law expr)))))
+(defn distr-pairs
+  [expr]
+  {:pre [(Or? expr)]}
+  (let [args-part1 (take-while not-and? (args expr))
+        args-part2 (drop-while not-and? (args expr))
+        first-and (first args-part2)]
+    (for [args-and (args first-and)
+          args-or (concat args-part1 (rest args-part2))]
+      [args-or args-and])))
+
+(defn apply-rules
+  [expr rules]
+  {:pre  [(expr? expr) (list? rules)]
+   :post [(expr? %)]}
+  (let [chosen-rule
+        (some
+          (fn [rule]
+            (if ((first rule) expr)
+              (second rule)
+              false))
+          rules)]
+    ;(println (str "Applying: " chosen-rule))
+    ;(println (str " to " expr))
+    (chosen-rule expr)))
+
+(declare impless)
+
+(defn impl-to-disj
+  "Represents implication as disjunction: (x -> y) = (!x | y)"
+  [expr]
+  {:pre  [(impl? expr)]
+   :post [(Or? %)]}
+  (let [[x y] (args expr)]
+    (Or (Not (impless x)) (impless y))))
+
+(def impless-rules
+  (list
+    [atom? identity]
+    [impl? impl-to-disj]
+    [Or? #(apply Or (map impless (args %)))]
+    [And? #(apply And (map impless (args %)))]
+    [Not? #(Not (impless (args %)))]))
+
+(defn impless
+  "Replaces all implications with disjunctions"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(expr? %)]}
+  (apply-rules expr impless-rules))
+
+(declare nnf)
+
+(defn const-negate
+  "Negates boolean const"
+  [expr]
+  {:pre  [(const? expr)]
+   :post [(const? %)]}
+  (if (const-value expr)
+    (const false)
+    (const true)))
+
+(defn const-negate?
+  "Tests whether expr is a negation of boolean constant"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (and
+    (Not? expr)
+    (const? (args expr))))
+
+(defn double-not?
+  "Tests whether expr is a negation of negation"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (and
+    (Not? expr)
+    (Not? (args expr))))
+
+(defn de-morgan-disj?
+  "Tests whether expr is a negation of disjunction"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (and
+    (Not? expr)
+    (Or? (args expr))))
+
+(defn de-morgan-conj?
+  "Tests whether expr is a negation of conjunction"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(boolean? %)]}
+  (and
+    (Not? expr)
+    (And? (args expr))))
+
+(defn apply-op
+  "Applies operation to expr args"
+  [op map-fn expr-args]
+  (apply op (map map-fn expr-args)))
+
+(defn apply-or
+  "Applies or operation to expr args"
+  [map-fn expr-args]
+  (apply-op Or map-fn expr-args))
+
+(defn apply-and
+  "Applies and operation to expr args"
+  [map-fn expr-args]
+  (apply-op And map-fn expr-args))
+
+(defn args-args
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(or (expr? %) (every? expr? %))]}
+  (args (args expr)))
+
+(def nnf-rules
+  (list
+    [not-impless? #(nnf (impless %))]
+    [atom? identity]
+    [const-negate? #(const-negate (args %))]
+    [double-not? #(nnf (args-args %))]
+    [de-morgan-disj? (fn [expr] (apply-and #(nnf (Not %)) (args-args expr)))]
+    [de-morgan-conj? (fn [expr] (apply-or #(nnf (Not %)) (args-args expr)))]
+    [And? #(apply-and nnf (args %))]
+    [Or? #(apply-or nnf (args %))]
+    ))
+
+(defn nnf
+  "Represents expr as negation normal form"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(expr? %)]}
+  (apply-rules expr nnf-rules))
+
+(declare cnf)
+
+(defn contains-const?
+  "Tests whether expr contains a constant"
+  ([expr]
+   {:pre  [(expr? expr)]
+    :post [(boolean? %)]}
+   (cond
+     (const? expr) true
+     (literal? expr) false
+     (Not? expr) (const? (args expr))
+     (Or? expr) (some? (some const? (args expr)))
+     (And? expr) (some? (some const? (args expr)))
+     (impl? expr) (some? (some const? (args expr)))))
+  ([expr constant]
+   {:pre  [(expr? expr) (const? constant)]
+    :post [(boolean? %)]}
+   (cond
+     (const? expr) (= expr constant)
+     (literal? expr) false
+     (Not? expr) (= (args expr) constant)
+     (Or? expr) (some? (some #(= % constant) (args expr)))
+     (And? expr) (some? (some #(= % constant) (args expr)))
+     (impl? expr) (some? (some #(= % constant) (args expr))))))
+
+(defn identity-law
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(expr? %)]}
+  (cond
+    (atom? expr) expr
+    (not (contains-const? expr)) expr
+    (Or? expr) (let [consts (map const-value (filter const? (args expr)))]
+                 (if (empty? consts)
+                   ()
+                   (if (some? (some identity consts))
+                     (const true)
+                     (if (empty? (filter #(not (const? %)) (args expr)))
+                       (const false)
+                       (Or (filter #(not (const? %)) (args expr)))))))
+    (Not? expr) (identity (args expr))
+
+    ))
+
+(defn identity-law
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(expr? %)]}
+  (cond
+    (and (And? expr) (contains-const? expr (const false))) (const false)
+    (and (Or? expr) (contains-const? expr (const true))) (const true)
+    (and (And? expr) (contains-const? expr (const true))) (apply And (filter #(not (= % (const true))) (args expr)))
+    (and (Or? expr) (contains-const? expr (const false))) (apply Or (filter #(not (= % (const false))) (args expr)))
+    :else expr))
+
+(def cnf-rules
+  (list
+    [not-nnf? #(cnf (nnf %))]
+    [atom? identity]
+    [contains-const? #(cnf (identity-law %))]
+    [elem-disj? identity]
+    [elem-conj? identity]
+    [assoc-disj? #(cnf (assoc-disj-law %))]
+    [assoc-conj? #(cnf (assoc-conj-law %))]
+    [distributive?
+     #(assoc-conj-law
+        (apply-and
+          (fn [[x y]] (cnf (assoc-disj-law (Or x y))))
+          (distr-pairs %)))]
+    [And? #(apply-and cnf (args %))]))
+
+(defn cnf
+  "Represents expr as conjunction normal form"
+  [expr]
+  {:pre  [(expr? expr)]
+   :post [(expr? %)]}
+  (let [res (apply-rules expr cnf-rules)]
+    (if (and (not (const? res)) (contains-const? res))
+      (cnf res)
+      res)))
+
+(defn recursive-assign
+  "Assigns const value to variable in expr"
+  [expr var val]
+  {:pre  [(expr? expr) (variable? var) (const? val)]
+   :post [(expr? %)]}
+  (cond
+    (and (variable? expr) (= expr var)) val
+    (Not? expr) (Not (recursive-assign (args expr) var val))
+    (And? expr) (apply And (map #(recursive-assign % var val) (args expr)))
+    (Or? expr) (apply Or (map #(recursive-assign % var val) (args expr)))
+    (impl? expr) (impl (recursive-assign (first (args expr)) var val) (recursive-assign (second (args expr)) var val))
+    :else expr))
+
+(defn assign
+  "Assigns const value to variable in expr"
+  [expr var val]
+  {:pre  [(expr? expr) (variable? var) (const? val)]
+   :post [(expr? %)]}
+  (cnf (recursive-assign expr var val)))
+
+
+
+
+
+
+
+
+
+
+
+
+; redundant
+
+(defn -main [])
+
+(defn same-variable?
+  "Tests whether var1 and var2 are the same"
+  [var1 var2]
+  {:pre  [(expr? var1) (expr? var2)]
+   :post [(boolean? %)]}
+  (= (variable-name var1) (variable-name var2)))
+
+(defn idempotent-disj-law
+  "(x | y | x) = (x | y)"
+  [expr]
+  {:pre  [(Or? expr)]
+   :post [(Or? %)]}
+  (apply Or (distinct (args expr))))
+
+(defn idempotent-conj-law
+  "(x & y & x) = (x & y)"
+  [expr]
+  {:pre  [(And? expr)]
+   :post [(And? %)]}
+  (apply And (distinct (args expr))))
+
+(defn identity-conj-law
+  "(true & x) = x, (false & x) = false"
+  [expr]
+  {:pre  [(And? expr)]
+   :post [(expr? %)]}
+  (let [c (const-value (first (args expr)))
+        e (rest (args expr))]
+    (if c
+      (apply And e)
+      (const c))))
+
+(defn identity-disj-law
+  "(true | x) = true, (false | x) = x"
+  [expr]
+  {:pre  [(Or? expr)]
+   :post [(expr? %)]}
+  (let [c (const-value (first (args expr)))
+        e (rest (args expr))]
+    (if c
+      (const c)
+      (apply Or e))))
--- test/clojure_course/task4_1/cnf_test.clj
+++ test/clojure_course/task4_1/cnf_test.clj
@@ -64,6 +64,15 @@
   (is (not (literal? (impl (variable :x) (variable :y)))))
   (is (thrown? AssertionError (literal? incorrect))))
 
+(deftest test-atom
+  (is (atom? (variable :x)))
+  (is (atom? (Not (variable :x))))
+  (is (atom? (const true)))
+  (is (not (atom? (And (variable :x) (variable :y)))))
+  (is (not (atom? (Or (variable :x) (variable :y)))))
+  (is (not (atom? (impl (variable :x) (variable :y)))))
+  (is (thrown? AssertionError (atom? incorrect))))
+
 (deftest test-elem-disj
   (is (elem-disj? (variable :x)))
   (is (elem-disj? (Not (variable :x))))
@@ -75,16 +84,63 @@
   (is (not (elem-disj? (impl (variable :x) (variable :y)))))
   (is (thrown? AssertionError (elem-disj? incorrect))))
 
+(deftest test-elem-conj
+  (is (elem-conj? (variable :x)))
+  (is (elem-conj? (Not (variable :x))))
+  (is (elem-conj? (And (variable :x))))
+  (is (elem-conj? (And (Not (variable :x)))))
+  (is (elem-conj? (And (variable :x) (Not (variable :y)) (variable :z))))
+  (is (not (elem-conj? (And (Or (variable :x) (Not (variable :y))) (variable :z)))))
+  (is (not (elem-conj? (Or (variable :x) (variable :y)))))
+  (is (not (elem-conj? (impl (variable :x) (variable :y)))))
+  (is (thrown? AssertionError (elem-conj? incorrect))))
+
+(deftest test-contains-const
+  (is (contains-const? (const false)))
+  (is (contains-const? (Not (const false))))
+  (is (contains-const? (And (const false) (variable :x))))
+  (is (contains-const? (Or (const false) (variable :z))))
+  (is (contains-const? (impl (const false) (impl (And (variable :x) (variable :y)) (Or (variable :z) (variable :w))))))
+  (is (not (contains-const? (variable :x))))
+  (is (not (contains-const? (Not (variable :x)))))
+  (is (not (contains-const? (And (Not (variable :x)) (variable :y)))))
+  (is (not (contains-const? (Or (And (variable :x) (Not (variable :y))) (variable :z)))))
+  (is (not (contains-const? (impl (Not (variable :m)) (impl (And (variable :x) (variable :y)) (Or (variable :z) (variable :w)))))))
+
+  (is (contains-const? (const false) (const false)))
+  (is (not (contains-const? (const false) (const true))))
+  (is (contains-const? (Not (const false)) (const false)))
+  (is (not (contains-const? (Not (const false)) (const true))))
+  (is (contains-const? (And (const false) (variable :x)) (const false)))
+  (is (not (contains-const? (And (const false) (variable :x)) (const true))))
+  (is (contains-const? (Or (const false) (variable :z)) (const false)))
+  (is (not (contains-const? (Or (const false) (variable :z)) (const true))))
+  (is (contains-const? (impl (const false) (impl (And (variable :x) (variable :y)) (Or (variable :z) (variable :w)))) (const false)))
+  (is (not (contains-const? (impl (const false) (impl (And (variable :x) (variable :y)) (Or (variable :z) (variable :w)))) (const true))))
+
+  (is (thrown? AssertionError (contains-const? incorrect))))
+
 (deftest test-cnf
   (is (cnf? (variable :x)))
   (is (cnf? (Not (variable :x))))
   (is (cnf? (And (variable :x) (Not (variable :y)) (variable :z))))
   (is (cnf? (Or (variable :x) (Not (variable :y)) (variable :z))))
-  (is (cnf? (And (Or (variable :x) (variable :y)) (Or (variable :z) (variable :w))))) ; (x | y) & (z | w)
-  (is (cnf? (And (variable :x) (Not (variable :y)) (Or (variable :z) (Not (variable :w)))))) ; x & !y & (z | !w)
-  (is (not (cnf? (Or (variable :x) (And (variable :y) (variable :z)))))) ; x | (y & z)
-  (is (not (cnf? (impl (variable :x) (variable :y)))))
-  (is (thrown? AssertionError (cnf? incorrect))))
+  (is (cnf? (And (Or (variable :x) (variable :y)) (Or (variable :z) (variable :w)))))
+  (is (cnf? (And (variable :x) (Not (variable :y)) (Or (variable :z) (Not (variable :w))))))
+  (is (not-cnf? (Or (variable :x) (And (variable :y) (variable :z)))))
+  (is (not-cnf? (impl (variable :x) (variable :y))))
+  (is (thrown? AssertionError (cnf? incorrect)))
+  (is (thrown? AssertionError (not-cnf? incorrect))))
+
+(deftest test-assoc-disj
+  (is (assoc-disj? (Or (variable :x) (Or (variable :y) (variable :z)))))
+  (is (not (assoc-disj? (Or (variable :x) (variable :y)))))
+  (is (thrown? AssertionError (assoc-disj? incorrect))))
+
+(deftest test-assoc-conj
+  (is (assoc-conj? (And (variable :x) (And (variable :y) (variable :z)))))
+  (is (not (assoc-conj? (And (variable :x) (variable :y)))))
+  (is (thrown? AssertionError (assoc-conj? incorrect))))
 
 (deftest test-expr-as-str
   (is (= "true" (expr-str (const true))))
@@ -95,22 +151,74 @@
   (is (= "(x -> y)" (expr-str (impl (variable :x) (variable :y)))))
   (is (= "(x -> (y | z))" (expr-str (impl (variable :x) (Or (variable :y) (variable :z)))))))
 
-(deftest test-impl-as-disj
-  (is (= "(!x | y)" (expr-str (impl-as-disj (impl (variable :x) (variable :y))))))
-  (is (= "(!(x | !y) | (z & !w))" (expr-str (impl-as-disj (impl (Or (variable :x) (Not (variable :y))) (And (variable :z) (Not (variable :w))))))))
-  (is (thrown? AssertionError (impl-as-disj incorrect)))
-  (is (thrown? AssertionError (impl-as-disj (Or (variable :x) (variable :y))))))
+(deftest test-assoc-disj-law
+  (is (= "(x | y | (z & w) | v | (m -> n))" (expr-str (assoc-disj-law (Or (Or (variable :x) (variable :y) (And (variable :z) (variable :w))) (variable :v) (impl (variable :m) (variable :n))))))))
 
-(deftest test-de-morgan-disj
-  (is (= "(!x & !y)" (expr-str (de-morgan-disj (Not (Or (variable :x) (variable :y)))))))
-  (is (thrown? AssertionError (de-morgan-disj incorrect))))
+(deftest test-assoc-conj-law
+  (is (= "(x & y & (z | w) & v & (m -> n))" (expr-str (assoc-conj-law (And (And (variable :x) (variable :y) (Or (variable :z) (variable :w))) (variable :v) (impl (variable :m) (variable :n))))))))
 
-(deftest test-de-morgan-conj
-  (is (= "(!x | !y)" (expr-str (de-morgan-conj (Not (And (variable :x) (variable :y)))))))
-  (is (thrown? AssertionError (de-morgan-conj incorrect))))
+(deftest test-idempotent-disj-law
+  (is (= "(x | y | (z & m))" (expr-str (idempotent-disj-law (Or (variable :x) (variable :y) (variable :x) (And (variable :z) (variable :m)) (And (variable :z) (variable :m))))))))
 
-(deftest test-associative-disj-law
-  (is (= "(x | y | (z & w) | v | (m -> n))" (expr-str (associative-disj-law (Or (Or (variable :x) (variable :y) (And (variable :z) (variable :w))) (variable :v) (impl (variable :m) (variable :n))))))))
+(deftest test-idempotent-conj-law
+  (is (= "(x & y & (z | m))" (expr-str (idempotent-conj-law (And (variable :x) (variable :y) (variable :x) (Or (variable :z) (variable :m)) (Or (variable :z) (variable :m))))))))
 
-(deftest test-associative-conj-law
-  (is (= "(x & y & (z | w) & v & (m -> n))" (expr-str (associative-conj-law (And (And (variable :x) (variable :y) (Or (variable :z) (variable :w))) (variable :v) (impl (variable :m) (variable :n))))))))
+(deftest test-impless
+  (is (= "(!x | y)" (expr-str (impless (impl (variable :x) (variable :y))))))
+  (is (= "(!(x | !y) | (z & !w))" (expr-str (impless (impl (Or (variable :x) (Not (variable :y))) (And (variable :z) (Not (variable :w))))))))
+  (is (= "(!!true | (!(x & y) | (z | w)))" (expr-str (impless (impl (Not (const true)) (impl (And (variable :x) (variable :y)) (Or (variable :z) (variable :w))))))))
+  (is (impless? (impless (impl (variable :x) (variable :y)))))
+  (is (impless? (impless (impl (Or (variable :x) (Not (variable :y))) (And (variable :z) (Not (variable :w)))))))
+  (is (impless? (impless (impl (Not (const true)) (impl (And (variable :x) (variable :y)) (Or (variable :z) (variable :w)))))))
+  (is (not-impless? (impl (variable :x) (variable :y))))
+  (is (not-impless? (impl (Or (variable :x) (Not (variable :y))) (And (variable :z) (Not (variable :w))))))
+  (is (not-impless? (impl (Not (const true)) (impl (And (variable :x) (variable :y)) (Or (variable :z) (variable :w))))))
+  (is (thrown? AssertionError (impless? incorrect))))
+
+(deftest test-const-negate
+  (is (const-value (const-negate (const false))))
+  (is (not (const-value (const-negate (const true)))))
+  (is (const-negate? (Not (const false))))
+  (is (not (const-negate? (const false))))
+  (is (thrown? AssertionError (const-negate incorrect)))
+  (is (thrown? AssertionError (const-negate? incorrect))))
+
+(deftest test-double-not
+  (is (double-not? (Not (Not (variable :x)))))
+  (is (not (double-not? (Not (variable :x)))))
+  (is (thrown? AssertionError (double-not? incorrect))))
+
+(deftest test-nnf
+  (is (= "x" (expr-str (nnf (Not (Not (variable :x)))))))
+  (is (= "(x & y)" (expr-str (nnf (Not (Not (And (variable :x) (variable :y))))))))
+  (is (= "(false | (x & y))" (expr-str (nnf (Or (Not (Not (Not (const true)))) (Not (Not (And (variable :x) (variable :y)))))))))
+  (is (nnf? (nnf (Not (Not (variable :x))))))
+  (is (nnf? (nnf (Not (Not (Not (const true)))))))
+  (is (nnf? (nnf (Not (Not (And (variable :x) (variable :y)))))))
+  (is (nnf? (nnf (Or (Not (Not (Not (const true)))) (Not (Not (And (variable :x) (variable :y))))))))
+  (is (nnf? (nnf (Not (impl (Not (const false)) (impl (And (variable :x) (variable :y)) (Or (And (variable :z) (variable :r)) (variable :w))))))))
+  (is (not-nnf? (Not (Not (variable :x)))))
+  (is (not-nnf? (Not (Not (Not (const true))))))
+  (is (not-nnf? (Not (Not (And (variable :x) (variable :y))))))
+  (is (not-nnf? (Or (Not (Not (Not (const true)))) (Not (Not (And (variable :x) (variable :y)))))))
+  (is (thrown? AssertionError (nnf incorrect)))
+  (is (thrown? AssertionError (nnf? incorrect)))
+  (is (thrown? AssertionError (not-nnf? incorrect))))
+
+(deftest test-to-cnf
+  (is (= "(x & y)" (expr-str (cnf (And (variable :x) (variable :y))))))
+  (is (= "((x | z) & (x | m))" (expr-str (cnf (Or (variable :x) (And (variable :z) (variable :m)))))))
+  (is (= "((x | z) & (x | m) & (y | z) & (y | m))" (expr-str (cnf (Or (And (variable :x) (variable :y)) (And (variable :z) (variable :m)))))))
+  (is (= "(x & y & (!z | !r) & !w)" (expr-str (cnf (Not (impl (Not (const false)) (impl (And (variable :x) (variable :y)) (Or (And (variable :z) (variable :r)) (variable :w)))))))))
+  (is (= "false" (expr-str (cnf (Not (impl (Not (const true)) (impl (And (variable :x) (variable :y)) (Or (And (variable :z) (variable :r)) (variable :w)))))))))
+  (is (cnf? (cnf (And (variable :x) (variable :y)))))
+  (is (cnf? (cnf (Or (variable :x) (And (variable :z) (variable :m))))))
+  (is (cnf? (cnf (Or (And (variable :x) (variable :y)) (And (variable :z) (variable :m))))))
+  (is (cnf? (cnf (Not (impl (Not (const false)) (impl (And (variable :x) (variable :y)) (Or (variable :z) (variable :w)))))))))
+
+(deftest test-assign
+  (is (= "y" (expr-str (assign (And (variable :x) (variable :y)) (variable :x) (const true)))))
+  (is (= "false" (expr-str (assign (And (variable :x) (variable :y)) (variable :x) (const false)))))
+  (is (= "true" (expr-str (assign (Or (Not (variable :x)) (Not (variable :y))) (variable :x) (const false)))))
+  (is (= "(x & y & !r & !w)" (expr-str (assign (Not (impl (Not (const false)) (impl (And (variable :x) (variable :y)) (Or (And (variable :z) (variable :r)) (variable :w))))) (variable :z) (const true)))))
+  (is (= "(x & y & !w)" (expr-str (assign (Not (impl (Not (const false)) (impl (And (variable :x) (variable :y)) (Or (And (variable :z) (variable :r)) (variable :w))))) (variable :z) (const false))))))
