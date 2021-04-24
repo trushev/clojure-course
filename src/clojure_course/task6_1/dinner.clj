4b086850e0befe3a8c75307e1d2655076595fac1
trushev <trushev.alex@gmail.com>
2021-04-25 02:31:30 +0700

    task6-1 - dining philosophers

--- src/clojure_course/task6_1/dinner.clj
+++ src/clojure_course/task6_1/dinner.clj
@@ -1,38 +1,44 @@
 (ns clojure-course.task6-1.dinner)
 
 (def fork-count 5)
-(def forks (map (fn [_] [(ref 0) (atom 0)]) (range fork-count)))
-(def fork-pairs
-  (cons
-    [(last forks) (first forks)]
-    (map
-      (fn [x y] [(nth forks x) (nth forks y)])
-      (range (dec fork-count))
-      (range 1 fork-count))))
+(def forks
+  (map
+    (fn [_] [(ref 0) (atom 0)])
+    (range fork-count)))
 
 (defn eat
-  [l-fork r-fork eat-time]
+  [l-fork r-fork think-time eat-time times]
   (fn []
-    (dosync
-      (swap! (last l-fork) inc)
-      (swap! (last r-fork) inc)
-      (alter (first l-fork) inc)
-      (alter (first r-fork) inc)
-      (Thread/sleep eat-time))))
+    (reduce
+      (fn [_ _]
+        (Thread/sleep think-time)
+        (dosync
+          (swap! (last l-fork) inc)
+          (swap! (last r-fork) inc)
+          (alter (first l-fork) inc)
+          (alter (first r-fork) inc)
+          (Thread/sleep eat-time)))
+      (range (inc times)))))
 
-(defn start
-  [eat-time]
-  (let [philosophers
+(defn dinner
+  [think-time eat-time times]
+  (let [fork-pairs
+        (cons
+          [(last forks) (first forks)]
+          (map
+            (fn [x y] [(nth forks x) (nth forks y)])
+            (range (dec fork-count))
+            (range 1 fork-count)))
+        philosophers
         (doall
           (map
             (fn [[l-fork r-fork]]
-              (new Thread (eat l-fork r-fork eat-time)))
+              (new Thread (eat l-fork r-fork think-time eat-time times)))
             fork-pairs))]
     (doall (map #(.start %) philosophers))
     (doall (map #(.join %) philosophers))))
 
 (defn -main []
   (println (map (fn [[r a]] [@r @a]) forks))
-  (time (start 100))
-  (println (map (fn [[r a]] [@r @a]) forks))
-  )
+  (time (dinner 10 100 1))
+  (println (map (fn [[r a]] [@r @a]) forks)))
