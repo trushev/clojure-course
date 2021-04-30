6d1da859589ebf7d2cbddd2f1abe8f50bc1d3b31
trushev <trushev.alex@gmail.com>
2021-04-30 17:25:28 +0700

    task6-1 - dining philosophers

--- src/clojure_course/task6_1/dinner.clj
+++ src/clojure_course/task6_1/dinner.clj
@@ -1,13 +1,27 @@
 (ns clojure-course.task6-1.dinner)
 
-(def fork-count 5)
-(def forks
+(defn- forks
+  [fork-count]
   (map
     (fn [_] [(ref 0) (atom 0)])
     (range fork-count)))
 
-(defn eat
-  [l-fork r-fork think-time eat-time times]
+(defn- print-forks
+  [forks]
+  (println (map (fn [[r a]] [@r @a]) forks))
+  forks)
+
+(defn- fork-pairs
+  [forks]
+  (cons
+    [(last forks) (first forks)]
+    (map
+      (fn [x y] [(nth forks x) (nth forks y)])
+      (range (dec (count forks)))
+      (range 1 (count forks)))))
+
+(defn- eat
+  [l-fork r-fork think-time eat-time piece-count]
   (fn []
     (reduce
       (fn [_ _]
@@ -18,27 +32,40 @@
           (alter (first l-fork) inc)
           (alter (first r-fork) inc)
           (Thread/sleep eat-time)))
-      (range (inc times)))))
+      (range (inc piece-count)))))
+
+(defn- philosophers
+  [fork-pairs think-time eat-time piece-count]
+  (doall
+    (map
+      (fn [[l-fork r-fork]]
+        (new
+          Thread
+          (eat l-fork r-fork think-time eat-time piece-count)))
+      fork-pairs)))
 
 (defn dinner
-  [think-time eat-time times]
-  (let [fork-pairs
-        (cons
-          [(last forks) (first forks)]
-          (map
-            (fn [x y] [(nth forks x) (nth forks y)])
-            (range (dec fork-count))
-            (range 1 fork-count)))
-        philosophers
-        (doall
-          (map
-            (fn [[l-fork r-fork]]
-              (new Thread (eat l-fork r-fork think-time eat-time times)))
-            fork-pairs))]
+  [fork-count think-time eat-time piece-count]
+  (let [forks (print-forks (forks fork-count))
+        philosophers (philosophers
+                       (fork-pairs forks)
+                       think-time
+                       eat-time
+                       piece-count)]
     (doall (map #(.start %) philosophers))
-    (doall (map #(.join %) philosophers))))
+    (doall (map #(.join %) philosophers))
+    forks))
+
+(def ^:private fork-count identity)
+(def ^:private think-time identity)
+(def ^:private eat-time identity)
+(def ^:private piece-count identity)
 
 (defn -main []
-  (println (map (fn [[r a]] [@r @a]) forks))
-  (time (dinner 10 100 1))
-  (println (map (fn [[r a]] [@r @a]) forks)))
+  (print-forks
+    (time
+      (dinner
+        (fork-count 5)
+        (think-time 10)
+        (eat-time 100)
+        (piece-count 1)))))
